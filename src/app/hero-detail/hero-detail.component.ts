1f03c9eda8fd5abbc387bb31e81a34decefa9a56
Hussam Alafandi <hosam.alafandi@gmail.com>
2020-10-10 17:47:03 +0200

    add go back button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -19,14 +19,18 @@ export class HeroDetailComponent implements OnInit {
     private location: Location
   ) { }
 
+  ngOnInit(): void {
+    this.getHero();
+  }
+
   getHero(): void{
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
         .subscribe(hero => this.hero = hero);
   }
 
-  ngOnInit(): void {
-    this.getHero();
+  goBack(): void{
+    this.location.back();
   }
 
 }

49a7f55b5fe4a47c0143969813ccb4790c6b260e
Hussam Alafandi <hosam.alafandi@gmail.com>
2020-10-10 17:25:04 +0200

    remove dead code onSelect() and selectedHero has no more use

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,22 +9,14 @@ import { MessageService } from "../message.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: selected hero id=${hero.id}');
-  }
-
   heroes: Hero[];
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void{
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-  
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
